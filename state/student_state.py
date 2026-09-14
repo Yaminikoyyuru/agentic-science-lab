6b9236a36e69f281c8f22a741ac7dbc0fb0f4677
Yaminikoyyuru <128447021+Yaminikoyyuru@users.noreply.github.com>
2026-09-14 22:27:43 +0530

    Update student_state.py

--- state/student_state.py
+++ state/student_state.py
@@ -2,28 +2,13 @@ class StudentState:
 
     def __init__(self):
 
-        # Current learning topic
         self.topic = ""
-
-        # Current question
         self.current_question = ""
-
-        # Student's prediction
         self.prediction = ""
-
-        # Current experiment
         self.experiment = {}
-
-        # All observations from the student
         self.observations = []
-
-        # All evaluator results
         self.evaluations = []
-
-        # Complete learning history
         self.learning_history = []
-
-        # Experiment counter
         self.experiment_number = 0
 
     def add_observation(self, observation):
@@ -37,6 +22,10 @@ class StudentState:
     def add_history(self, data):
 
         self.learning_history.append(data)
+
+    def set_prediction(self, prediction):
+
+        self.prediction = prediction
 
     def next_experiment(self):
 
